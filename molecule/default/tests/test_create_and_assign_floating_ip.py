266cdbd360d845e84193416a9221b510add9e2da
phongdly <phongdly@gmail.com>
2018-07-10 13:14:44 -0700

    (ASC-707) updated nova tests to use pytest-rpc instead of local utils.py

--- molecule/default/tests/test_create_and_assign_floating_ip.py
+++ molecule/default/tests/test_create_and_assign_floating_ip.py
@@ -1,7 +1,8 @@
-import utils
+import pytest_rpc.helpers as helpers
 import os
 import pytest
 import testinfra.utils.ansible_runner
+from time import sleep
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('os-infra_hosts')[:1]
@@ -9,31 +10,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 utility_container = ("lxc-attach -n $(lxc-ls -1 | grep utility | head -n 1) "
                      "-- bash -c '. /root/openrc ; ")
 
-random_str = utils.generate_random_string(6)
+random_str = helpers.generate_random_string(6)
 instance_name = "test_instance_{}".format(random_str)
 image_name = 'Cirros-0.3.5'
-getway_net = 'GATEWAY_NET'
+gateway_net = 'GATEWAY_NET'
 private_net = 'PRIVATE_NET'
 flavor = 'm1.tiny'
-floating_ip_name = None
-
-
-def create_instance_from_image(run_on_host):
-    """Create a test instance from image"""
-    image_id = utils.get_id_by_name('image', image_name, run_on_host)
-    assert image_id is not None
-
-    network_id = utils.get_id_by_name('network', private_net, run_on_host)
-    assert network_id is not None
-
-    cmd = "{} openstack server create --image {} --flavor {} --nic net-id={} {}'".format(utility_container, image_id,
-                                                                                         flavor, network_id,
-                                                                                         instance_name)
-    run_on_host.run_expect([0], cmd)
-
-    assert (utils.verify_asset_in_list('server', instance_name, run_on_host))
-    assert (utils.get_expected_value('server', instance_name, 'status', 'ACTIVE', run_on_host))
-    assert (utils.get_expected_value('server', instance_name, 'OS-EXT-STS:power_state', 'Running', run_on_host))
+floating_ip = None
 
 
 @pytest.mark.test_id('a97e1202-796a-11e8-ba13-525400bd8005')
@@ -42,12 +25,12 @@ def create_instance_from_image(run_on_host):
 def test_create_floating_ip(host):
     """Create floating IP"""
 
-    global floating_ip_name
-    floating_ip_name = utils.create_floating_ip(getway_net, host)
-    assert (floating_ip_name is not None)
+    global floating_ip
+    floating_ip = helpers.create_floating_ip(gateway_net, host)
+    assert floating_ip
 
     # Before being assigned, the floating IP status should be 'DOWN'
-    assert (utils.get_expected_value('floating ip', floating_ip_name, 'status', 'DOWN', host))
+    assert (helpers.get_expected_value('floating ip', floating_ip, 'status', 'DOWN', host))
 
 
 @pytest.mark.test_id('ab24ffbd-798b-11e8-a2b2-6c96cfdb2e43')
@@ -56,21 +39,39 @@ def test_create_floating_ip(host):
 def test_assign_floating_ip_to_instance(host):
     """Assign floating IP to an instance/server"""
 
-    create_instance_from_image(host)
+    # Creating an instance from image
+    data = {
+        "instance_name": instance_name,
+        "from_source": 'image',
+        "source_name": image_name,
+        "flavor": flavor,
+        "network_name": private_net,
+    }
 
-    assert (floating_ip_name is not None)  # floating_ip_name is same as the floating IP address
+    helpers.create_instance(data, host)
 
-    instance_id = utils.get_id_by_name('server', instance_name, host)
-    assert (instance_id is not None)
+    # TODO: will find out a better way to avoid implicit sleep. 'status' is 'ACTIVE' is not enough to ensure the
+    # TODO: instance is ready, there are many instance statuses that might cause the test failed.
+    # TODO: run `openstack server show <instance-ID> -f json` to see all the states
+    sleep(120)
 
-    cmd = "{} openstack server add floating ip {} {}'".format(utility_container, instance_id, floating_ip_name)
+    # Verify the new instance is ACTIVE and Running.
+    assert (helpers.get_expected_value('server', instance_name, 'status', 'ACTIVE', host, 20))
+    assert (helpers.get_expected_value('server', instance_name, 'OS-EXT-STS:power_state', 'Running', host, 20))
+
+    assert floating_ip
+
+    instance_id = helpers.get_id_by_name('server', instance_name, host)
+    assert instance_id
+
+    cmd = "{} openstack server add floating ip {} {}'".format(utility_container, instance_id, floating_ip)
 
     host.run_expect([0], cmd)
 
     # After being assigned, the floating IP status should be 'ACTIVE'
-    assert (utils.get_expected_value('floating ip', floating_ip_name, 'status', 'ACTIVE', host))
+    assert (helpers.get_expected_value('floating ip', floating_ip, 'status', 'ACTIVE', host))
 
-    assert (utils.ping_ip_from_host(floating_ip_name, host))
+    assert (helpers.ping_ip_from_utility_container(floating_ip, host))
 
 
 @pytest.mark.test_id('e3985ee1-798b-11e8-ad0e-6c96cfdb2e43')
@@ -79,5 +80,5 @@ def test_assign_floating_ip_to_instance(host):
 def test_teardown(host):
     """delete server and floating ip created in the tests"""
 
-    utils.delete_it('server', instance_name, host)
-    utils.delete_it('floating ip', floating_ip_name, host)
+    helpers.delete_it('server', instance_name, host)
+    helpers.delete_it('floating ip', floating_ip, host)
